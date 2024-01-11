ac20730a7037baadbe081f00e1f9f74efd7feb48
Adarsh Manakkalath Muraleedharan <am22aea@herts.ac.uk>
2024-01-11 03:37:06 +0000

    WebCam Working

--- test_code.py
+++ test_code.py
@@ -19,3 +19,31 @@ plt.imshow(frame)
 
 #releases the device from the use
 capture.release()
+
+def click_photo():
+    capture = cv2.VideoCapture(0)
+    ret, frame = capture.read()
+    cv2.imwrite('webcamphoto.jpg', frame)
+    capture.release()
+
+#To click just a picture use this function
+#click_photo()
+
+#To get the complete Webcam as Video like how a webcam works
+capture = cv2.VideoCapture(0)
+
+#in a loop of frames until we force quit
+while capture.isOpened():
+    ret, frame = capture.read()
+
+    #show image
+    cv2.imshow('WebCam', frame)
+
+    #check whether the key q has been pressed for force quit
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+#release webcam
+capture.release()
+#close all windows that is the shown frame
+cv2.destroyAllWindows()
